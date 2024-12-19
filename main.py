efef4093409c6af318df419cf2e1d21e794b845e
TheFlyingPiano99 <simonzolika1999@gmail.com>
2024-12-19 19:34:53 +0100

    Reading MIDI file

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 import markov_music.markov_model as markov_model
 import markov_music.midi_layer as midi_layer
 
+
 def main():
     print('Hi, Markov Music!')
-    midi = midi_layer.MidiLayer("resources/midi/Boci boci tarka.mid")
+    midi = midi_layer.MidiData("resources/midi/Boci boci tarka.mid")
     model = markov_model.MarkovModel()
+
 
 if __name__ == '__main__':
     main()
--- markov_music/midi_layer.py
+++ markov_music/midi_layer.py
@@ -1,6 +1,11 @@
-import mido
+import mido     # Source: https://mido.readthedocs.io/en/stable/index.html
 import pathlib
 
-class MidiLayer:
+class MidiData:
     def __init__(self, file_path: pathlib.Path):
-        pass
+        file = mido.MidiFile(file_path)
+        for i, track in enumerate(file.tracks):
+            print('Track {}: {}'.format(i, track.name))
+            for msg in track:
+                print(msg)
+
